e3da6be3d53a0d442dc66ea0db05af31daea278e
Samuel Adelowo <samueladelowo92@gmail.com>
2023-03-03 14:38:48 +0100

    updating logic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
+const posts = [];
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -17,15 +19,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {homeStartingContent});
+  res.render("home", { homeStartingContent, posts });
+  // console.log(posts);
+});
+
+app.get("/posts/:postTitle", (req, res) => {
+  posts.forEach(function (post) {
+    if (_.lowerCase(req.params.postTitle) == _.lowerCase(post.title)) {
+      console.log("Match Found");
+    }
+  });
+
+  // for (i = 0; i < posts.length; i++) {
+  //   if (req.params.postTitle == posts[i].title) {
+  //     console.log("Match Found");
+  //   }
+  // }
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {aboutContent});
+  res.render("about", { aboutContent });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {contactContent});
+  res.render("contact", { contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
